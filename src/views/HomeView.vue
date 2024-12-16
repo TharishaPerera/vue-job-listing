3e8d1ef30bda7cceb9f2b5a262be02b7b366017a
TharishaPerera <tharishaperera@gmail.com>
2024-12-16 20:20:15 +0530

    completed application

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCard from "@/components/HomeCard.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
+
+<template>
+    <Hero title="Test Title Updated" subtitle="test subtitle" />
+    <HomeCard />
+    <JobListings :limit="3" :show-button="true" />
+</template>
